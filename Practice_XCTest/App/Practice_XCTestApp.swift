4d454bce8e8413c3a297e37b74f81237e86b4d81
Jiho <yjh7827@naver.com>
2024-08-09 10:22:58 +0900

    feat: base setting

---
+++ Practice_XCTest/App/Practice_XCTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Practice_XCTestApp.swift
+//  Practice_XCTest
+//
+//  Created by 유지호 on 8/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Practice_XCTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Practice_XCTest/View/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Practice_XCTest
+//
+//  Created by 유지호 on 8/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
